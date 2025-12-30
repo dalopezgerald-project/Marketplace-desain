5622af173116479e9c8609b9fbd400847e93b782
dalopegerald-project <emailkamu@gmail.com>
2025-12-30 23:24:56 +0700

    Add routes for service CRUD and admin approval

--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,17 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::middleware(['auth','role:desainer'])->group(function(){
+    Route::get('/desainer/service',[ServiceController::class,'index']);
+    Route::get('/desainer/service/create',[ServiceController::class,'create']);
+    Route::post('/desainer/service',[ServiceController::class,'store']);
+});
+
+Route::middleware(['auth','role:admin'])->group(function(){
+    Route::get('/admin/dashboard',[AdminController::class,'dashboard']);
+    Route::post('/admin/service/{id}/approve',[AdminController::class,'approve']);
+    Route::post('/admin/service/{id}/reject',[AdminController::class,'reject']);
+});
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
